a2e9361c219b2cf959df345d52f44dcd7efbbf9d
SalihTalha <SalihTalha@github.com>
2019-11-16 15:28:45 +0300

    cv2.cvtColor added

--- build/IntroductionToImageProcessing/1-AccessingColors.py
+++ build/IntroductionToImageProcessing/1-AccessingColors.py
@@ -3,6 +3,9 @@ import numpy as np
 
 img = cv2.imread('./../../data/elma.jpg',1) #Intr.->build->create.->data->elma.jpg
 
+convert = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+
 #       elma.jpg için genişlik:640 yükseklik:512 pixel
 height, width, depth = img.shape # 640,512,3
 #       Kontrol et -> print(width)
@@ -22,6 +25,6 @@ img[:,:,1] = 255
 # : = 0:height, 0:width
 
 cv2.imshow("Image",img)
-
+cv2.imshow("HSV", convert)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
