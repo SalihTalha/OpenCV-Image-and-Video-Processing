8dd64d5a10cd4dee2139104e7d0533435693b8e1
SalihTalha <SalihTalha@github.com>
2019-11-16 22:04:26 +0300

    Little mistake fixed

--- build/IntroductionToImageProcessing/5-Blurring.py
+++ build/IntroductionToImageProcessing/5-Blurring.py
@@ -12,14 +12,14 @@ dst = cv2.filter2D(img,-1,kernel)
 dest = cv2.blur(dst,(9,9))
 blur = cv2.GaussianBlur(img,(9,9),1)
 median = cv2.medianBlur(img,5)
-bilateral = cv2.bilateralFilter()
+bilateral = cv2.bilateralFilter(img, 7, 50, 50)
 
 # print(dest)
 # print(dst)
 
 plt.subplot(121), plt.imshow(dst), plt.title('Original')
 plt.xticks([]), plt.yticks([])
-plt.subplot(122), plt.imshow(dest), plt.title('Blur')
+plt.subplot(122), plt.imshow(bilateral), plt.title('Blur')
 plt.xticks([]), plt.yticks([])
 
 plt.show()
--- build/IntroductionToImageProcessing/7-EdgeDetection.py
+++ build/IntroductionToImageProcessing/7-EdgeDetection.py
@@ -26,6 +26,8 @@ Blurred = cv2.GaussianBlur(img,(3,3),0)
 
 edge = cv2.filter2D(img,0,kernelY)
 edgeBlurred = cv2.filter2D(Blurred,0,kernelY)
+
+
 cv2.imshow("Edge Detection After Blur",edgeBlurred)
 cv2.imshow("Original",img)
 cv2.imshow("EdgeDetection",edge)
